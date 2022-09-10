23e70baa29f2087b480164d3ff19d7fa989341eb
Wei Liu <newway.liu@gmail.com>
2022-09-10 15:20:09 +0100

    added retry

--- tide_time.py
+++ tide_time.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib
 import http
-from tenacity import retry
-from tenacity import stop_after_attempt, wait_exponential
 from urllib.request import urlopen
 import urllib.request
 import sqlite3
@@ -12,15 +10,14 @@ from tide_record import TideRecord
 import logging
 import random
 import configparser
-import time
+from time import sleep
 from multiprocessing import Pool, cpu_count
 
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                     datefmt='%d-%m-%Y:%H:%M:%S', level=logging.DEBUG)
 
 BASE_URL = "https://www.bbc.co.uk/weather/coast-and-sea/tide-tables/"
-TIDE_TABLE = 'tidal'
-LOCATION_TABLE = 'location'
+RETRY = 3
 USER_AGENT_LIST = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
@@ -50,8 +47,8 @@ def get_db_connection(config):
     return sqlite3.connect(config['DEFAULT']['Database'])
 
 
-def get_tide_location_detail_to_map(db_connection) -> dict[str, tuple[str, int]]:
-    sql = 'SELECT port_id, region_id, name FROM ' + LOCATION_TABLE
+def get_tide_location_detail_to_map(db_connection, table_name) -> dict[str, tuple[str, int]]:
+    sql = 'SELECT port_id, region_id, name FROM ' + table_name
     port_id_2_detail_map = {}
     cursor = db_connection.cursor()
     cursor.execute(sql)
@@ -105,56 +102,62 @@ def insert(db_connection, table_name, tide_record: TideRecord):
                                     tide_height))
     db_connection.commit()
 
-# retry 5 times in case of bad network connection
-# https://github.com/jd/tenacity/issues/147 doesn't work with multiprocessing pool
-@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=1, max=32))
+
 def parse_record(region_id, port_id, port_name):
     location_code = str(region_id) + '/' + port_id
     url = BASE_URL + location_code
     current_year = datetime.now().year
     current_month = datetime.now().month
     current_day = datetime.now().day
-    tide_record_list = list()
-    try:
-        req = urllib.request.Request(
-            url,
-            data=None,
-            headers={"Content-Type": "application/json; charset=utf-8",
-                     "User-Agent": f"{random.choice(USER_AGENT_LIST)}"
-                     }
-        )
+    daily_tide_record_list = list()
+    retry = 0
+    failed = True
+    while retry < RETRY and failed:
+        try:
+            req = urllib.request.Request(
+                url,
+                data=None,
+                headers={"Content-Type": "application/json; charset=utf-8",
+                         "User-Agent": f"{random.choice(USER_AGENT_LIST)}"
+                         }
+            )
 
-        html = urllib.request.urlopen(req)
-        soup = BeautifulSoup(html, features="html.parser")
-        tables = soup.select("table.wr-c-tide-extremes")
-        logging.info(f"{len(tables)} days predictions found for {port_name} (port_id={port_id})")
-        # each table contains tide prediction for 1 day starting 'today'
-        for offset, table in enumerate(tables):
-            row_text = table.select_one("caption").text
-            types = [[td.text for td in row.find_all("th")] for row in table.select("tr")]
-            data = [[td.text for td in row.find_all("td")] for row in table.select("tr")[1:]]
-            _, time, height = types.pop(0)
-            high_low = [x[0] for x in types]
-            logging.debug(f"{len(high_low)} {row_text}")
-            # BST or UTC ?
-            # TODO handle timezone consistency
-            timezone = 'BST' if 'BST' in time else 'UTC'
-            t_record = TideRecord(current_year, current_month, current_day, offset,
-                                  timezone, region_id, port_id)
-            for type, item in zip(high_low, data):
-                height = float(item[1])
-                t_record.add_record(type, item[0], height)
-            tide_record_list.append(t_record)
+            html = urllib.request.urlopen(req)
+            soup = BeautifulSoup(html, features="html.parser")
+            tables = soup.select("table.wr-c-tide-extremes")
+            logging.info(f"{len(tables)} days predictions found for {port_name} (port_id={port_id})")
+            # each table contains tide prediction for 1 day starting 'today'
+            for offset, table in enumerate(tables):
+                row_text = table.select_one("caption").text
+                types = [[td.text for td in row.find_all("th")] for row in table.select("tr")]
+                data = [[td.text for td in row.find_all("td")] for row in table.select("tr")[1:]]
+                _, time, height = types.pop(0)
+                high_low = [x[0] for x in types]
+                logging.debug(f"{len(high_low)} {row_text}")
+                # BST or UTC ?
+                # TODO handle timezone consistency
+                timezone = 'BST' if 'BST' in time else 'UTC'
+                t_record = TideRecord(current_year, current_month, current_day, offset,
+                                      timezone, region_id, port_id)
+                for type, item in zip(high_low, data):
+                    height = float(item[1])
+                    t_record.add_record(type, item[0], height)
+                daily_tide_record_list.append(t_record)
 
-    except urllib.error.HTTPError as e:
-        logging.error(f"{url} not found")
-        raise e
-    except http.client.IncompleteRead as ine:
-        logging.error(str(ine))
-        raise ine
-    finally:
-        # logging.debug(f"{parse_record.retry.statistics['attempt_number']}")
-        return tide_record_list
+        except urllib.error.HTTPError as e:
+            logging.error(f"{url} not found")
+            failed = True
+            retry += 1
+            sleep(retry**2)
+            continue
+        except http.client.IncompleteRead as ine:
+            logging.error(str(ine))
+            failed = True
+            retry += 1
+            sleep(retry**2)
+            continue
+        break
+    return port_id, daily_tide_record_list
 
 
 @click.command()
@@ -164,15 +167,13 @@ def parse_record(region_id, port_id, port_name):
               help='port-ids to scrape, if not specified all available ports will be scraped.')
 @click.option('-n', '--num-workers', type=int, default=cpu_count(),
               help=f'num of concurrent workers, default {cpu_count()}')
-
-# @click.option('-s', '--sleep', type=int, default=0, help="sleep this many seconds between each location each worker")
 def main(config_file: str, port_id: str, num_workers: int):
 
     config = read_config(config_file)
     conn = get_db_connection(config)
     create_tide_table_if_not_exist(conn, config.get('DEFAULT', 'Tide_table'))
     create_location_table_if_not_exist(conn, config.get('DEFAULT', 'Location_table'))
-    port_id_2_detail_map = get_tide_location_detail_to_map(conn)
+    port_id_2_detail_map = get_tide_location_detail_to_map(conn, config.get('DEFAULT', 'Location_table'))
 
     # if port_id is not set, fetch all available port_ids from db
     if not port_id:
@@ -185,13 +186,23 @@ def main(config_file: str, port_id: str, num_workers: int):
 
     with Pool(processes=num_workers) as pool:
         cnt = 0
+        successful_pid = 0
+        failed_pids = list()
         logging.debug(f"{num_workers} workers/processes")
-        for records in pool.starmap(parse_record, args):
-            cnt += len(records)
-            for record in records:
+        for pid, tide_predictions in pool.starmap(parse_record, args):
+            num_predictions = len(tide_predictions)
+            if num_predictions > 0:
+                successful_pid += 1
+            else:
+                failed_pids.append(pid)
+            cnt += len(tide_predictions)
+            for record in tide_predictions:
                 insert(conn, config.get('DEFAULT', 'Tide_table'), record)
 
-        logging.info(f"Total {cnt} records saved")
+        logging.info(f"{successful_pid}/{len(port_id)} locations collected, total {cnt} records saved")
+        if failed_pids:
+            logging.warning(f"failed locations port ids: {failed_pids}")
+
     conn.close()
 
 
